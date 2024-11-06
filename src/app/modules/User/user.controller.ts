912b745cb1f20617e9b7a7eed80a43b178142320
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-11-06 07:37:08 +0600

    Created a service to create an admin

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,9 +2,21 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  //console.log(req.body);
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    //console.log(req.body);
+    const result = await userService.createAdmin(req.body);
+    res.status(200).json({
+      success: true,
+      message: "Admin Created successfuly!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err?.name || "Something went wrong",
+      error: err,
+    });
+  }
 };
 
 export const userController = {
