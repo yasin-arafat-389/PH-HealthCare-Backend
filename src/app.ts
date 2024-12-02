a2114ab21157263961002b6b1bf723f009a0c41c
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-12-02 11:27:28 +0600

    Created server

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
-import { userRoutes } from "./app/modules/User/user.route";
+import router from "./app/routes";
+import { StatusCodes } from "http-status-codes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
 
 const app: Application = express();
 app.use(cors());
@@ -11,10 +13,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send({
-    Message: "Ph health care server is up and running...",
+    Message: "Ph health care server..",
   });
 });
 
-app.use("/api/v1/user", userRoutes);
+app.use("/api/v1", router);
+
+app.use(globalErrorHandler);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(StatusCodes.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
 
 export default app;
