8cfc893ad612b75a27ff211193c7ff44fbf673ca
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-12-16 08:49:39 +0600

    Created a global error handler

---
+++ src/app/middlewares/globalErrorHandler.ts
@@ -0,0 +1,33 @@
+import { Prisma } from "@prisma/client";
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+
+const globalErrorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = err.message || "Something went wrong!";
+  let error = err;
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation Error";
+    error = err.message;
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Duplicate Key error";
+      error = err.meta;
+    }
+  }
+
+  res.status(statusCode).json({
+    success,
+    message,
+    error,
+  });
+};
+
+export default globalErrorHandler;
