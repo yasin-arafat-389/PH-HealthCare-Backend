2bf01e78257fd1c26f381787af152e9a186fe2d4
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2024-12-27 08:27:51 +0600

    Created a service to create appointment

---
+++ src/app/modules/Appointment/appointment.service.ts
@@ -0,0 +1,97 @@
+import prisma from "../../../shared/prisma";
+import { IAuthUser } from "../../interfaces/common";
+import { v4 as uuidv4 } from "uuid";
+import { IPaginationOptions } from "../../interfaces/pagination";
+import {
+  AppointmentStatus,
+  PaymentStatus,
+  Prisma,
+  UserRole,
+} from "@prisma/client";
+import { paginationHelper } from "../../../helpers/paginationHelper";
+import ApiError from "../../errors/ApiError";
+import { StatusCodes } from "http-status-codes";
+
+const createAppointment = async (user: IAuthUser, payload: any) => {
+  const patientData = await prisma.patient.findUniqueOrThrow({
+    where: {
+      email: user?.email,
+    },
+  });
+
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: payload.doctorId,
+    },
+  });
+
+  await prisma.doctorSchedules.findFirstOrThrow({
+    where: {
+      doctorId: doctorData.id,
+      scheduleId: payload.scheduleId,
+      isBooked: false,
+    },
+  });
+
+  const videoCallingId: string = uuidv4();
+
+  const result = await prisma.$transaction(async (tx) => {
+    const appointmentData = await tx.appointment.create({
+      data: {
+        patientId: patientData.id,
+        doctorId: doctorData.id,
+        scheduleId: payload.scheduleId,
+        videoCallingId,
+      },
+      include: {
+        patient: true,
+        doctor: true,
+        schedule: true,
+      },
+    });
+
+    await tx.doctorSchedules.update({
+      where: {
+        doctorId_scheduleId: {
+          doctorId: doctorData.id,
+          scheduleId: payload.scheduleId,
+        },
+      },
+      data: {
+        isBooked: true,
+        appointmentId: appointmentData.id,
+      },
+    });
+
+    // PH-HealthCare-datatime
+    const today = new Date();
+
+    const transactionId =
+      "PH-HealthCare-" +
+      today.getFullYear() +
+      "-" +
+      today.getMonth() +
+      "-" +
+      today.getDay() +
+      "-" +
+      today.getHours() +
+      "-" +
+      today.getMinutes();
+
+    await tx.payment.create({
+      data: {
+        appointmentId: appointmentData.id,
+        amount: doctorData.appointmentFee,
+        transactionId,
+      },
+    });
+
+    return appointmentData;
+  });
+
+  return result;
+};
+
+export const AppointmentService = {
+  createAppointment,
+};
