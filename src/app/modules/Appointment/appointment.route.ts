cee01fedee4efe064983537668bf274d60478c05
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-01-06 08:26:58 +0600

    Created appointment routes

---
+++ src/app/modules/Appointment/appointment.route.ts
@@ -0,0 +1,34 @@
+import express from "express";
+import { AppointmentController } from "./appointment.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import validateRequest from "../../middlewares/validateRequest";
+import { AppointmentValidation } from "./appointment.validation";
+
+const router = express.Router();
+
+router.get(
+  "/",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN),
+  AppointmentController.getAllFromDB
+);
+
+router.get(
+  "/my-appointment",
+  auth(UserRole.PATIENT, UserRole.DOCTOR),
+  AppointmentController.getMyAppointment
+);
+
+router.post(
+  "/",
+  auth(UserRole.PATIENT),
+  validateRequest(AppointmentValidation.createAppointment),
+  AppointmentController.createAppointment
+);
+
+router.patch(
+  "/status/:id",
+  auth(UserRole.SUPER_ADMIN, UserRole.ADMIN, UserRole.DOCTOR),
+  AppointmentController.changeAppointmentStatus
+);
+export const AppointmentRoutes = router;
