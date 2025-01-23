442b666f402d81735299d21a35cb45c01c8e9ca0
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-01-23 08:06:22 +0600

    Created a service to update doctor info into database

--- src/app/modules/Doctor/doctor.service.ts
+++ src/app/modules/Doctor/doctor.service.ts
@@ -105,7 +105,71 @@ const getByIdFromDB = async (id: string): Promise<Doctor | null> => {
   return result;
 };
 
+const updateIntoDB = async (id: string, payload: IDoctorUpdate) => {
+  const { specialties, ...doctorData } = payload;
+
+  const doctorInfo = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id,
+    },
+  });
+
+  await prisma.$transaction(async (transactionClient) => {
+    await transactionClient.doctor.update({
+      where: {
+        id,
+      },
+      data: doctorData,
+    });
+
+    if (specialties && specialties.length > 0) {
+      // delete specialties
+      const deleteSpecialtiesIds = specialties.filter(
+        (specialty) => specialty.isDeleted
+      );
+      //console.log(deleteSpecialtiesIds)
+      for (const specialty of deleteSpecialtiesIds) {
+        await transactionClient.doctorSpecialties.deleteMany({
+          where: {
+            doctorId: doctorInfo.id,
+            specialitiesId: specialty.specialtiesId,
+          },
+        });
+      }
+
+      // create specialties
+      const createSpecialtiesIds = specialties.filter(
+        (specialty) => !specialty.isDeleted
+      );
+      console.log(createSpecialtiesIds);
+      for (const specialty of createSpecialtiesIds) {
+        await transactionClient.doctorSpecialties.create({
+          data: {
+            doctorId: doctorInfo.id,
+            specialitiesId: specialty.specialtiesId,
+          },
+        });
+      }
+    }
+  });
+
+  const result = await prisma.doctor.findUnique({
+    where: {
+      id: doctorInfo.id,
+    },
+    include: {
+      doctorSpecialties: {
+        include: {
+          specialities: true,
+        },
+      },
+    },
+  });
+  return result;
+};
+
 export const DoctorService = {
+  updateIntoDB,
   getAllFromDB,
   getByIdFromDB,
 };
