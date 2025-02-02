5293d898918a5d5250052786869b07caeea36103
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-02-02 07:28:57 +0600

    Created a service to get all dosctor schedules

--- src/app/modules/DoctorSchedule/doctorSchedule.service.ts
+++ src/app/modules/DoctorSchedule/doctorSchedule.service.ts
@@ -146,8 +146,78 @@ const deleteFromDB = async (user: IAuthUser, scheduleId: string) => {
   return result;
 };
 
+const getAllFromDB = async (
+  filters: IDoctorScheduleFilterRequest,
+  options: IPaginationOptions
+) => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      doctor: {
+        name: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      },
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    if (
+      typeof filterData.isBooked === "string" &&
+      filterData.isBooked === "true"
+    ) {
+      filterData.isBooked = true;
+    } else if (
+      typeof filterData.isBooked === "string" &&
+      filterData.isBooked === "false"
+    ) {
+      filterData.isBooked = false;
+    }
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  const whereConditions: any =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+  const result = await prisma.doctorSchedules.findMany({
+    include: {
+      doctor: true,
+      schedule: true,
+    },
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {},
+  });
+  const total = await prisma.doctorSchedules.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const DoctorScheduleService = {
   insertIntoDB,
   getMySchedule,
   deleteFromDB,
+  getAllFromDB,
 };
