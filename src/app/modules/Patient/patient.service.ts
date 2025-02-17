09700c59598779b3a034b3adb40153a2808d80c1
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-02-17 07:37:59 +0600

    Created a service to get all patients from database

---
+++ src/app/modules/Patient/patient.service.ts
@@ -0,0 +1,77 @@
+import { Patient, Prisma, UserStatus } from "@prisma/client";
+import prisma from "../../../shared/prisma";
+import { IPatientFilterRequest, IPatientUpdate } from "./patient.interface";
+import { IPaginationOptions } from "../../interfaces/pagination";
+import { paginationHelper } from "../../../helpers/paginationHelper";
+import { patientSearchableFields } from "./patient.constant";
+
+const getAllFromDB = async (
+  filters: IPatientFilterRequest,
+  options: IPaginationOptions
+) => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(options);
+  const { searchTerm, ...filterData } = filters;
+
+  const andConditions = [];
+
+  if (searchTerm) {
+    andConditions.push({
+      OR: patientSearchableFields.map((field) => ({
+        [field]: {
+          contains: searchTerm,
+          mode: "insensitive",
+        },
+      })),
+    });
+  }
+
+  if (Object.keys(filterData).length > 0) {
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => {
+        return {
+          [key]: {
+            equals: (filterData as any)[key],
+          },
+        };
+      }),
+    });
+  }
+  andConditions.push({
+    isDeleted: false,
+  });
+
+  const whereConditions: Prisma.PatientWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.patient.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: "desc",
+          },
+    include: {
+      medicalReport: true,
+      patientHealthData: true,
+    },
+  });
+  const total = await prisma.patient.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
+export const PatientService = {
+  getAllFromDB,
+};
