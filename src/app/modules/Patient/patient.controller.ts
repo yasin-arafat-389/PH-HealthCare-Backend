472aa44abbcc0252717ac77a3a287634af02fcd9
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-02-26 08:09:45 +0600

    Created controller to soft delete patient

--- src/app/modules/Patient/patient.controller.ts
+++ src/app/modules/Patient/patient.controller.ts
@@ -56,9 +56,21 @@ const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
+const softDelete = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await PatientService.softDelete(id);
+  sendResponse(res, {
+    statusCode: StatusCodes.OK,
+    success: true,
+    message: "Patient soft deleted successfully",
+    data: result,
+  });
+});
+
 export const PatientController = {
   getAllFromDB,
   getByIdFromDB,
   updateIntoDB,
   deleteFromDB,
+  softDelete,
 };
