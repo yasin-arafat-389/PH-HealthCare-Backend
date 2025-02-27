bde55d313e590c13df124b9c7a5ff22024f66642
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-02-27 08:25:33 +0600

    Created routes for patient module

---
+++ src/app/modules/Patient/patient.route.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import { PatientController } from "./patient.controller";
+
+const router = express.Router();
+
+router.get("/", PatientController.getAllFromDB);
+
+router.get("/:id", PatientController.getByIdFromDB);
+
+router.patch("/:id", PatientController.updateIntoDB);
+
+router.delete("/:id", PatientController.deleteFromDB);
+
+router.delete("/soft/:id", PatientController.softDelete);
+
+export const PatientRoutes = router;
