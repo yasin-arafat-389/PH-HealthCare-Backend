61ab514b502e42c0b2f154b255ddeb8501348604
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-03-01 08:15:53 +0600

    Created a service to initiate payment

---
+++ src/app/modules/Payment/payment.service.ts
@@ -0,0 +1,36 @@
+import prisma from "../../../shared/prisma";
+import { PaymentStatus } from "@prisma/client";
+import { SSLService } from "../SSLCOMMERZ/sslcommerz.service";
+
+const initPayment = async (appointmentId: string) => {
+  const paymentData = await prisma.payment.findFirstOrThrow({
+    where: {
+      appointmentId,
+    },
+    include: {
+      appointment: {
+        include: {
+          patient: true,
+        },
+      },
+    },
+  });
+
+  const initPaymentData = {
+    amount: paymentData.amount,
+    transactionId: paymentData.transactionId,
+    name: paymentData.appointment.patient.name,
+    email: paymentData.appointment.patient.email,
+    address: paymentData.appointment.patient.address,
+    phoneNumber: paymentData.appointment.patient.contactNumber,
+  };
+
+  const result = await SSLService.initPayment(initPaymentData);
+  return {
+    paymentUrl: result.GatewayPageURL,
+  };
+};
+
+export const PaymentService = {
+  initPayment,
+};
