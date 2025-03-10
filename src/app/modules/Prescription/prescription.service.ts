e0cef145443bb4a5b9bd1bbdd6d1f182af1a4b01
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-03-10 10:03:11 +0600

    Created a service to get all prescriptions

--- src/app/modules/Prescription/prescription.service.ts
+++ src/app/modules/Prescription/prescription.service.ts
@@ -92,7 +92,62 @@ const patientPrescription = async (
   };
 };
 
+const getAllFromDB = async (filters: any, options: IPaginationOptions) => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(options);
+  const { patientEmail, doctorEmail } = filters;
+  const andConditions = [];
+
+  if (patientEmail) {
+    andConditions.push({
+      patient: {
+        email: patientEmail,
+      },
+    });
+  }
+
+  if (doctorEmail) {
+    andConditions.push({
+      doctor: {
+        email: doctorEmail,
+      },
+    });
+  }
+
+  const whereConditions: Prisma.PrescriptionWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.prescription.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: "desc",
+          },
+    include: {
+      doctor: true,
+      patient: true,
+      appointment: true,
+    },
+  });
+  const total = await prisma.prescription.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const PrescriptionService = {
   insertIntoDB,
   patientPrescription,
+  getAllFromDB,
 };
