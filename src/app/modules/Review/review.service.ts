ab1b503792a1071a1609b026541b5b5656b60f0c
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-03-17 11:36:28 +0600

    Created a service to fetch all reviews

--- src/app/modules/Review/review.service.ts
+++ src/app/modules/Review/review.service.ts
@@ -56,6 +56,61 @@ const insertIntoDB = async (user: IAuthUser, payload: any) => {
   });
 };
 
+const getAllFromDB = async (filters: any, options: IPaginationOptions) => {
+  const { limit, page, skip } = paginationHelper.calculatePagination(options);
+  const { patientEmail, doctorEmail } = filters;
+  const andConditions = [];
+
+  if (patientEmail) {
+    andConditions.push({
+      patient: {
+        email: patientEmail,
+      },
+    });
+  }
+
+  if (doctorEmail) {
+    andConditions.push({
+      doctor: {
+        email: doctorEmail,
+      },
+    });
+  }
+
+  const whereConditions: Prisma.ReviewWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.review.findMany({
+    where: whereConditions,
+    skip,
+    take: limit,
+    orderBy:
+      options.sortBy && options.sortOrder
+        ? { [options.sortBy]: options.sortOrder }
+        : {
+            createdAt: "desc",
+          },
+    include: {
+      doctor: true,
+      patient: true,
+      //appointment: true,
+    },
+  });
+  const total = await prisma.review.count({
+    where: whereConditions,
+  });
+
+  return {
+    meta: {
+      total,
+      page,
+      limit,
+    },
+    data: result,
+  };
+};
+
 export const ReviewService = {
   insertIntoDB,
+  getAllFromDB,
 };
