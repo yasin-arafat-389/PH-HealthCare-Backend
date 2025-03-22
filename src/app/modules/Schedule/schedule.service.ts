0e8bfd12a2bf4091f7415a3678bbff4c99bafb9b
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-03-22 09:31:43 +0600

    Created a service to insert schedule into database

---
+++ src/app/modules/Schedule/schedule.service.ts
@@ -0,0 +1,72 @@
+import { addHours, addMinutes, format } from "date-fns";
+import prisma from "../../../shared/prisma";
+import { Prisma, Schedule } from "@prisma/client";
+import { IPaginationOptions } from "../../interfaces/pagination";
+import { IAuthUser } from "../../interfaces/common";
+import { IFilterRequest, ISchedule } from "./schedule.interface";
+import { paginationHelper } from "../../../helpers/paginationHelper";
+
+const inserIntoDB = async (payload: ISchedule): Promise<Schedule[]> => {
+  const { startDate, endDate, startTime, endTime } = payload;
+
+  const interverlTime = 30;
+
+  const schedules = [];
+
+  const currentDate = new Date(startDate); // start date
+  const lastDate = new Date(endDate); // end date
+
+  while (currentDate <= lastDate) {
+    // 09:30  ---> ['09', '30']
+    const startDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(startTime.split(":")[0])
+        ),
+        Number(startTime.split(":")[1])
+      )
+    );
+
+    const endDateTime = new Date(
+      addMinutes(
+        addHours(
+          `${format(currentDate, "yyyy-MM-dd")}`,
+          Number(endTime.split(":")[0])
+        ),
+        Number(endTime.split(":")[1])
+      )
+    );
+
+    while (startDateTime < endDateTime) {
+      const scheduleData = {
+        startDateTime: startDateTime,
+        endDateTime: addMinutes(startDateTime, interverlTime),
+      };
+
+      const existingSchedule = await prisma.schedule.findFirst({
+        where: {
+          startDateTime: scheduleData.startDateTime,
+          endDateTime: scheduleData.endDateTime,
+        },
+      });
+
+      if (!existingSchedule) {
+        const result = await prisma.schedule.create({
+          data: scheduleData,
+        });
+        schedules.push(result);
+      }
+
+      startDateTime.setMinutes(startDateTime.getMinutes() + interverlTime);
+    }
+
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return schedules;
+};
+
+export const ScheduleService = {
+  inserIntoDB,
+};
