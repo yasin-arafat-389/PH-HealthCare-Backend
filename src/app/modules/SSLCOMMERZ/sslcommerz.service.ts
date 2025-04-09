6e39b88e17edb5e428ce6b114771924a94a2beda
yasin-arafat-389 <yasin.arafat.rwd@gmail.com>
2025-04-09 18:21:01 +0600

    Created a service to validate payment

--- src/app/modules/SSLCOMMERZ/sslcommerz.service.ts
+++ src/app/modules/SSLCOMMERZ/sslcommerz.service.ts
@@ -52,6 +52,20 @@ const initPayment = async (paymentData: IPaymentData) => {
   }
 };
 
+const validatePayment = async (payload: any) => {
+  try {
+    const response = await axios({
+      method: "GET",
+      url: `${config.ssl.sslValidationApi}?val_id=${payload.val_id}&store_id=${config.ssl.storeId}&store_passwd=${config.ssl.storePass}&format=json`,
+    });
+
+    return response.data;
+  } catch (err) {
+    throw new ApiError(StatusCodes.BAD_REQUEST, "Payment validation failed!");
+  }
+};
+
 export const SSLService = {
   initPayment,
+  validatePayment,
 };
